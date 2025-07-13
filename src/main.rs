5db289e4925b5dedda454b8b9b95258b9d90dea2
misut <pkr5207@gmail.com>
2025-07-13 21:36:56 +0900

    feat: macroquad implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+#[macroquad::main("Rhytry")]
+async fn main() {
+    loop {
+        clear_background(BLACK);
+
+        draw_text("Hello from macroquad!", 20.0, 40.0, 30.0, WHITE);
+
+        next_frame().await;
+    }
 }
