a263a12628bc35df1614b9fc219f09d73155881a
misut <pkr5207@gmail.com>
2025-07-25 21:53:25 +0900

    feat: scroll onpu and hit it

--- src/infrastructure/bevy/onpu.rs
+++ src/infrastructure/bevy/onpu.rs
@@ -1,22 +1,98 @@
-use bevy::prelude::*;
+use std::collections::VecDeque;
+
+use bevy::{ecs::system::Query, prelude::*, render::camera::Viewport};
 
 use crate::{infrastructure::bevy::app::AppState, infrastructure::bevy::assets::TextureAssets};
 
 pub struct OnpuPlugin;
 
 #[derive(Component)]
-pub struct Onpu;
+enum Onpu {
+    Don,
+    Kat,
+}
+
+#[derive(Resource, Default)]
+pub struct OnpuQueue(pub VecDeque<Entity>);
 
 impl Plugin for OnpuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(AppState::Playing), spawn_onpu);
+        app.init_resource::<OnpuQueue>()
+            .add_systems(OnEnter(AppState::Playing), (setup_taiko, spawn_onpu))
+            .add_systems(Update, (hit_onpu, move_onpu, push_onpu_to_queue));
+    }
+}
+
+fn setup_taiko(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Single<&Window>,
+) {
+    let window_size = window.resolution.physical_size().as_vec2();
+
+    // Initialize centered, non-window-filling viewport
+    commands.spawn((
+        Camera2d,
+        Camera {
+            viewport: Some(Viewport {
+                physical_position: UVec2::new(0, 0),
+                physical_size: window_size.as_uvec2(),
+                ..default()
+            }),
+            ..default()
+        },
+    ));
+
+    commands.spawn((
+        Mesh2d(meshes.add(Rectangle::new(50000.0, window_size.y * 0.3))),
+        MeshMaterial2d(materials.add(Color::linear_rgb(0.01, 0.01, 0.1))),
+        Transform::from_translation(Vec3::new(0.0, 0.0, 0.0)),
+    ));
+}
+
+fn push_onpu_to_queue(query: Query<Entity, Added<Onpu>>, mut queue: ResMut<OnpuQueue>) {
+    for onpu in query.iter() {
+        queue.0.push_back(onpu);
     }
 }
 
 fn spawn_onpu(mut commands: Commands, textures: Res<TextureAssets>) {
     commands.spawn((
         Sprite::from_image(textures.don.clone()),
-        Transform::from_translation(Vec3::new(0., 0., 1.0)),
-        Onpu,
+        Transform::from_translation(Vec3::new(600., 0., 0.)),
+        Onpu::Don,
     ));
+
+    commands.spawn((
+        Sprite::from_image(textures.kat.clone()),
+        Transform::from_translation(Vec3::new(900., 0., 0.)),
+        Onpu::Kat,
+    ));
+
+    commands.spawn((
+        Sprite::from_image(textures.don.clone()),
+        Transform::from_translation(Vec3::new(1200., 0., 0.)),
+        Onpu::Don,
+    ));
+}
+
+fn move_onpu(time: Res<Time>, mut positions: Query<(&mut Onpu, &mut Transform)>) {
+    for (mut _onpu, mut transform) in &mut positions {
+        transform.translation.x -= 200.0 * time.delta_secs();
+    }
+}
+
+fn hit_onpu(
+    mut commands: Commands,
+    mut onpu_queue: ResMut<OnpuQueue>,
+    input: Res<ButtonInput<KeyCode>>,
+) {
+    if input.just_pressed(KeyCode::Space) {
+        if let Some(onpu) = onpu_queue.0.pop_front() {
+            commands.entity(onpu).despawn();
+        } else {
+            println!("No more onpus to hit!");
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-use bevy::{
-    asset::AssetMetaCheck, color::palettes::css::GREEN, math::ops::powf, prelude::*,
-    render::camera::Viewport,
-};
+use bevy::{asset::AssetMetaCheck, prelude::*};
 use rhytry::infrastructure::bevy::app::AppPlugin;
 
 fn main() {
@@ -22,136 +19,14 @@ fn main() {
         )
         .add_plugins(AppPlugin)
         .add_systems(Startup, setup)
-        .add_systems(FixedUpdate, controls)
         .run();
-}
-
-fn controls(
-    mut camera_query: Query<(&mut Camera, &mut Transform, &mut Projection)>,
-    window: Query<&Window>,
-    input: Res<ButtonInput<KeyCode>>,
-    time: Res<Time<Fixed>>,
-) {
-    let Ok(window) = window.single() else {
-        return;
-    };
-    let Ok((mut camera, mut transform, mut projection)) = camera_query.single_mut() else {
-        return;
-    };
-    let fspeed = 600.0 * time.delta_secs();
-    let uspeed = fspeed as u32;
-    let window_size = window.resolution.physical_size();
-
-    // Camera movement controls
-    if input.pressed(KeyCode::ArrowUp) {
-        transform.translation.y += fspeed;
-    }
-    if input.pressed(KeyCode::ArrowDown) {
-        transform.translation.y -= fspeed;
-    }
-    if input.pressed(KeyCode::ArrowLeft) {
-        transform.translation.x -= fspeed;
-    }
-    if input.pressed(KeyCode::ArrowRight) {
-        transform.translation.x += fspeed;
-    }
-
-    // Camera zoom controls
-    if let Projection::Orthographic(projection2d) = &mut *projection {
-        if input.pressed(KeyCode::Comma) {
-            projection2d.scale *= powf(4.0f32, time.delta_secs());
-        }
-
-        if input.pressed(KeyCode::Period) {
-            projection2d.scale *= powf(0.25f32, time.delta_secs());
-        }
-    }
-
-    if let Some(viewport) = camera.viewport.as_mut() {
-        // Viewport movement controls
-        if input.pressed(KeyCode::KeyW) {
-            viewport.physical_position.y = viewport.physical_position.y.saturating_sub(uspeed);
-        }
-        if input.pressed(KeyCode::KeyS) {
-            viewport.physical_position.y += uspeed;
-        }
-        if input.pressed(KeyCode::KeyA) {
-            viewport.physical_position.x = viewport.physical_position.x.saturating_sub(uspeed);
-        }
-        if input.pressed(KeyCode::KeyD) {
-            viewport.physical_position.x += uspeed;
-        }
-
-        // Bound viewport position so it doesn't go off-screen
-        viewport.physical_position =
-            viewport.physical_position.min(window_size - viewport.physical_size);
-
-        // Viewport size controls
-        if input.pressed(KeyCode::KeyI) {
-            viewport.physical_size.y = viewport.physical_size.y.saturating_sub(uspeed);
-        }
-        if input.pressed(KeyCode::KeyK) {
-            viewport.physical_size.y += uspeed;
-        }
-        if input.pressed(KeyCode::KeyJ) {
-            viewport.physical_size.x = viewport.physical_size.x.saturating_sub(uspeed);
-        }
-        if input.pressed(KeyCode::KeyL) {
-            viewport.physical_size.x += uspeed;
-        }
-
-        // Bound viewport size so it doesn't go off-screen
-        viewport.physical_size = viewport
-            .physical_size
-            .min(window_size - viewport.physical_position)
-            .max(UVec2::new(20, 20));
-    }
 }
 
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
-    window: Single<&Window>,
 ) {
-    let window_size = window.resolution.physical_size().as_vec2();
-
-    // Initialize centered, non-window-filling viewport
-    commands.spawn((
-        Camera2d,
-        Camera {
-            viewport: Some(Viewport {
-                physical_position: (window_size * 0.125).as_uvec2(),
-                physical_size: (window_size * 0.75).as_uvec2(),
-                ..default()
-            }),
-            ..default()
-        },
-    ));
-
-    // Create a minimal UI explaining how to interact with the example
-    commands.spawn((
-        Text::new(
-            "Move the mouse to see the circle follow your cursor.\n\
-                    Use the arrow keys to move the camera.\n\
-                    Use the comma and period keys to zoom in and out.\n\
-                    Use the WASD keys to move the viewport.\n\
-                    Use the IJKL keys to resize the viewport.",
-        ),
-        Node {
-            position_type: PositionType::Absolute,
-            top: Val::Px(12.0),
-            left: Val::Px(12.0),
-            ..default()
-        },
-    ));
-
-    // Add mesh to make camera movement visible
-    commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(40.0, 20.0))),
-        MeshMaterial2d(materials.add(Color::from(GREEN))),
-    ));
-
     // Add background to visualize viewport bounds
     commands.spawn((
         Mesh2d(meshes.add(Rectangle::new(50000.0, 50000.0))),
